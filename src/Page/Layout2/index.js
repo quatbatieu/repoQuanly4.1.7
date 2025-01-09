25f4638ef6e0dc10541465088df357b14a0169df
ThanhNha3 <nhant2003@gmail.com>
2025-01-09 15:31:50 +0700

    FE - Thêm crypto news

--- src/Page/Layout2/index.js
+++ src/Page/Layout2/index.js
@@ -1,21 +1,30 @@
-import { ArrowUpOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import {
+  ArrowUpOutlined,
+  PlusCircleOutlined,
+  SearchOutlined,
+  TagOutlined,
+} from "@ant-design/icons";
 import {
   Avatar,
   Button,
   Card,
   Col,
+  Image,
   Input,
+  List,
   Progress,
   Row,
   Select,
   Table,
+  Tag,
   Typography,
 } from "antd";
-import React from "react";
+import React, { useState } from "react";
 
 import "./Layout2.scss";
 import CoinPriceCard from "components/Layout2Components/CoinPriceCard";
 import BalanceHistoryChart from "components/Layout2Components/BalanceHistoryChart";
+import IMAGE_STORES from "../../assets/icons/gamestore.png"
 
 const cardData = [
   {
@@ -52,30 +61,76 @@ const cardData = [
   },
 ];
 
-const CheckBoxList = [
+const newsData = [
   {
-    id: 1,
-    title: "Meeting with me",
+    title: "10 things you must know before trading in cryptocurrency",
+    tags: ["BTC", "Crypto", "Trading", "Tips", "Cryptocurrency"],
+    image:
+      "https://th.bing.com/th/id/OIP.woMHlD1B10-b0FQINwjlWQHaE8?rs=1&pid=ImgDetMain",
+    description:
+      "Khám phá 10 điều quan trọng bạn cần biết trước khi bắt đầu giao dịch tiền điện tử. Từ việc hiểu rủi ro đến cách chọn sàn giao dịch, bài viết này sẽ giúp bạn trang bị kiến thức cần thiết để bắt đầu hành trình giao dịch an toàn và hiệu quả.",
   },
   {
-    id: 2,
-    title: "Hangout with friends",
+    title: "Getting started with cryptocurrency - what is blockchain",
+    tags: ["BTC", "Crypto", "Trading", "Tips", "Cryptocurrency"],
+    image:
+      "https://th.bing.com/th/id/OIP.woMHlD1B10-b0FQINwjlWQHaE8?rs=1&pid=ImgDetMain",
+    description:
+      "Bắt đầu với tiền điện tử có thể là một thách thức, nhưng với kiến thức đúng đắn về blockchain – công nghệ nền tảng của các loại tiền số, bạn sẽ nắm bắt được cách hoạt động và tiềm năng của thị trường này. Tìm hiểu tất cả trong bài viết này",
   },
   {
-    id: 3,
-    title: "Meeting with teams",
+    title: "Is cryptocurrency investment a trap or opportunity?",
+    tags: ["BTC", "Crypto", "Trading", "Tips", "Cryptocurrency"],
+    image:
+      "https://th.bing.com/th/id/OIP.SgsFZrww5zr3j_TxECspfgHaEK?rs=1&pid=ImgDetMain",
+    description:
+      "Đầu tư vào tiền điện tử luôn là chủ đề tranh luận. Trong bài viết này, chúng tôi phân tích các cơ hội và rủi ro tiềm ẩn, giúp bạn đưa ra quyết định thông minh hơn về việc liệu đầu tư vào tiền điện tử là cơ hội vàng hay một cái bẫy nguy hiểm.",
+  },
+];
+
+const orderData = [
+  {
+    key: "1",
+    currency: "0.24 BTC",
+    rate: "1 BTC = $740",
+    date: "08.10.17",
+    fee: "-$2.33",
   },
   {
-    id: 4,
-    title: "Daily Report",
+    key: "2",
+    currency: "0.34 RPL",
+    rate: "1 RPL = $80.2",
+    date: "08.03.17",
+    fee: "-$1.23",
+  },
+  // Add more data as needed
+];
+
+const columns = [
+  {
+    title: "Currency",
+    dataIndex: "currency",
+    key: "currency",
   },
   {
-    id: 5,
-    title: "Self Learning",
+    title: "Rate (USD)",
+    dataIndex: "rate",
+    key: "rate",
+  },
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "Fee",
+    dataIndex: "fee",
+    key: "fee",
   },
 ];
 
 const Layout2 = () => {
+  const [currentCryptoTab, setCurrentCryptoTab] = useState("All");
   return (
     <div>
       <Row gutter={[24, 24]} className="mb-3">
@@ -116,7 +171,6 @@ const Layout2 = () => {
               </div>
             </div>
 
-            {/* Right Section */}
             <div className="flex-grow-1 border-left pl-3">
               <Typography.Title level={5}>
                 Portfolio Distribution
@@ -263,40 +317,140 @@ const Layout2 = () => {
         </Col>
         <Col xl={8} lg={12} md={12} sm={24} xs={24}>
           <div className="bg-white shadow-lg rounded full-height p-3">
-              <Typography.Title level={4}>Currency Calculator</Typography.Title>
-              <Typography.Text>1.87 BTC equals</Typography.Text>
-              <Typography.Title level={2} className="text-primary my-2">
-                11466.78 USD
-              </Typography.Title>
-              <Typography.Text type="secondary">
-                @ 1 BTC = 6178.72 USD
-              </Typography.Text>
-              <div style={{ marginTop: "20px" }}>
-                <Row gutter={16}>
-                  <Col span={8}>
-                    <label>From</label>
-                    <Select defaultValue="BTC">
-                      <Select.Option value="BTC">BTC</Select.Option>
-                      <Select.Option value="ETH">ETH</Select.Option>
-                    </Select>
-                  </Col>
-                  <Col span={8}>
-                    <label>To</label>
-                    <Select defaultValue="BTC">
-                      <Select.Option value="BTC">BTC</Select.Option>
-                      <Select.Option value="ETH">ETH</Select.Option>
-                    </Select>
-                  </Col>
-                  <Col span={8}>
-                    <label>Amount (BTC):</label>
-                    <Input placeholder="0.0" />
-                  </Col>
-                </Row>
-                <Button type="primary" block style={{ marginTop: "20px" }}>
-                  Transfer Now
-                </Button>
+            <Typography.Title level={4}>Currency Calculator</Typography.Title>
+            <Typography.Text>1.87 BTC equals</Typography.Text>
+            <Typography.Title level={2} className="text-primary my-2">
+              11466.78 USD
+            </Typography.Title>
+            <Typography.Text type="secondary">
+              @ 1 BTC = 6178.72 USD
+            </Typography.Text>
+            <div style={{ marginTop: "20px" }}>
+              <Row gutter={16}>
+                <Col span={8}>
+                  <label>From</label>
+                  <Select defaultValue="BTC">
+                    <Select.Option value="BTC">BTC</Select.Option>
+                    <Select.Option value="ETH">ETH</Select.Option>
+                  </Select>
+                </Col>
+                <Col span={8}>
+                  <label>To</label>
+                  <Select defaultValue="BTC">
+                    <Select.Option value="BTC">BTC</Select.Option>
+                    <Select.Option value="ETH">ETH</Select.Option>
+                  </Select>
+                </Col>
+                <Col span={8}>
+                  <label>Amount (BTC):</label>
+                  <Input placeholder="0.0" />
+                </Col>
+              </Row>
+              <Button type="primary" block style={{ marginTop: "20px" }}>
+                Transfer Now
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={[24, 24]} className="equal-height-row mb-3 height">
+        <Col xl={16} lg={24} md={24} sm={24} xs={24}>
+          <div className="bg-white shadow-lg rounded full-height p-3">
+            <div className="d-flex justify-content-between align-items-center mb-3">
+              <div>
+                <Typography.Text>Crypto News</Typography.Text>
+              </div>
+              <div className="d-flex justify-content-between align-items-center">
+                {["All", "BTC", "ETH", "XRP"].map((tab, index) => {
+                  return (
+                    <div>
+                      <Button
+                        type="link"
+                        key={index}
+                        className={
+                          currentCryptoTab == tab
+                            ? "bg-primary text-white"
+                            : "text-primary"
+                        }
+                        onClick={() => setCurrentCryptoTab(tab)}
+                      >
+                        {tab}
+                      </Button>
+                    </div>
+                  );
+                })}
+              </div>
+              <div>
+                <SearchOutlined className="fs-5 text-primary" />
               </div>
             </div>
+            <List
+              itemLayout="vertical"
+              dataSource={newsData}
+              renderItem={(item) => (
+                <List.Item>
+                  <div className="d-flex flex-wrap gap-3">
+                    <Image
+                      className="rounded"
+                      alt={item.title}
+                      src={item.image}
+                      style={{ maxWidth: "100px", height: "auto" }}
+                    />
+                    <div style={{ flex: 1 }}>
+                      <Typography.Title level={5}>
+                        {item.title}
+                      </Typography.Title>
+                      <div className="mb-3">{item.description}</div>
+                      <div className="d-flex justify-content-between align-items-center flex-wrap">
+                        <div className="d-flex gap-2 flex-wrap text-secondary align-items-center">
+                          <TagOutlined />
+                          {item.tags.reduce((acc, current) => {
+                            return acc + "" + current + ", ";
+                          }, "")}
+                        </div>
+                        <Button type="link">Read Full Story</Button>
+                      </div>
+                    </div>
+                  </div>
+                </List.Item>
+              )}
+            />
+          </div>
+        </Col>
+        <Col xl={8} lg={24} md={24} sm={24} xs={24}>
+          <div
+            className="d-flex justify-content-between shadow-lg rounded p-3"
+            style={{
+              background:
+                "linear-gradient(90deg,rgb(154, 30, 226),rgb(54, 43, 218))",
+            }}
+          >
+            <div className="d-flex flex-column gap-3">
+              <Typography.Text className="text-white" level={4}>
+                Download Mobile Apps
+              </Typography.Text>
+              <Typography.Text strong className="text-white" level={3}>
+                Now, your account is on your finger
+              </Typography.Text>
+            </div>
+            <div>
+              <img
+                style={{ width: "100px" }}
+                src={IMAGE_STORES}
+              />
+            </div>
+          </div>
+          <div className="bg-white shadow-lg rounded full-height p-3">
+            <div className="d-flex justify-content-between align-items-center">
+              <h2>Order History</h2>
+              <Button type="link">Detailed History</Button>
+            </div>
+            <Table
+              dataSource={orderData}
+              columns={columns}
+              pagination={false}
+            />
+          </div>
         </Col>
       </Row>
     </div>
